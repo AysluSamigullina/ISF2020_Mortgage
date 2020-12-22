7cb42b72841063cd19eabd5cfb28a40697c6ad0c
Самигуллина Айслу <ayslu.samigullina@gazprombank.ru>
2020-12-22 15:58:35 +0300

    Lesson 4 Доработки #4

--- src/main/java/ru/isf/mortgage/config/SpringConfig.java
+++ src/main/java/ru/isf/mortgage/config/SpringConfig.java
@@ -1,20 +1,12 @@
 package ru.isf.mortgage.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.Executor;
 
 @Component
 @ComponentScan("ru.isf.mortgage")
 @EnableAsync
 public class SpringConfig {
-
-    @Bean(name = "threadPoolTaskExecutor")
-    public Executor threadPoolTaskExecutor() {
-        return new ThreadPoolTaskExecutor();
-    }
 }
